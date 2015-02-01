4c2cf4b40ecccdd2671f3893153e621ee92209a8
Kyle Baker <finesttype@gmail.com>
2015-02-01 03:05:15 -0600

    Lots of visual updates, some js and css updates.
    decided on a new font, added functionality to button that causes it to
    be 25% opacity when a cookie is not able to be made from ingredients in
    pot, but trying to improve, commented out working version and have 'fix'
    live, doesn't fully work yet. Still seems to work as well as prior
    version, though. Font switcher button only applies to H2's, and
    functions. Cleaned up some spare CSS/html stuff.

--- cookieapp.js
+++ cookieapp.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-/*--------------------Step 2:*/
+/*Step 2:*/
 var inventory = {
   product: {
     money: 1000,
@@ -15,14 +15,34 @@ var inventory = {
   }
 };
 
+var cookiecheck = function() {
+	if (inventory.pot.sugar<3 && inventory.pot.flour<6){
+		$('#bake-one').fadeTo('fast',.25)
+	}
+	else if (inventory.pot.sugar>2 && inventory.pot.flour>5){
+		$('#bake-one').fadeTo('.1',.50)
+	}
+};
+
+cookiecheck();
 
 $('.button').mouseenter(function(){
-	$(this).fadeTo('50',1)
+	$(this).fadeTo('fast',1)
 })
 
+/*is broken, doesn't make #bake-one fade down to .25 like it should, seems the if condition is never met*/
+var $bakeone = $('#bake-one')
 $('.button').mouseleave(function(){
-	$(this).fadeTo('fast',.5)
+	if ($('this') == $('#bake-one')){cookiecheck()}
+	else {$(this).fadeTo('.1',.5)}
 })
+
+/* Working version of mouseleave, but has 'stepped fading' that is undesirable
+$('.button').mouseleave(function(){
+	$(this).fadeTo('.1',.5)
+	cookiecheck();
+})
+*/
 
 $('#use-sugar').click(function(){
 	if (inventory.ingredients.sugar>0){
@@ -32,7 +52,7 @@ $('#use-sugar').click(function(){
 	else{
 		alert("You're out of sugar, you should buy some more!")
 	}
-})
+});
 
 $('#buy-sugar').click(function(){
 	if(inventory.product.money>9){
@@ -44,7 +64,7 @@ $('#buy-sugar').click(function(){
 	else {
 		("Dude, you're out of money. Game over.")
 	}
-})
+});
 
 $('#use-flour').click(function(){
 	if(inventory.ingredients.flour>0){
@@ -54,7 +74,7 @@ $('#use-flour').click(function(){
 	else{
 		alert("You're out of flour, you should buy some more!")
 	}
-})
+});
 
 $('#buy-flour').click(function(){
 	if(inventory.product.money>14){
@@ -65,7 +85,7 @@ $('#buy-flour').click(function(){
 	else {
 		alert("Dude, you're out of money. Game over.")
 	}
-})
+});
 
 $('#bake-one').click(function(){
 	if (inventory.pot.flour>5 && inventory.pot.sugar>2){
@@ -74,7 +94,17 @@ $('#bake-one').click(function(){
 		inventory.product.cookies ++
 		alert("You made a cookie!")
 	}
+	else if (inventory.pot.flour==0&&inventory.pot.sugar==0){
+		alert("What are you trying to do? Don't you know how to follow a recipe?! Right now you don't have anything in the pot!")
+	}
 
+	else if (inventory.pot.flour==1){
+		alert("What are you trying to do? Don't you know how to follow a recipe?! Right now you only have " + inventory.pot.flour + " unit of flour, but you need six in there!")
+	}
+
+	else if (inventory.pot.sugar==1){
+		alert("What are you trying to do? Don't you know how to follow a recipe?! Right now you only have " + inventory.pot.sugar + " unit of flour, but you need six in there!")
+	}
 	else if (inventory.pot.flour<6){
 		alert("What are you trying to do? Don't you know how to follow a recipe?! Right now you only have " + inventory.pot.flour + " units of flour, but you need six in there!")
 	}
@@ -82,39 +112,46 @@ $('#bake-one').click(function(){
 	else if (inventory.pot.sugar<3){
 		alert("What are you trying to do? Don't you know how to follow a recipe?! Right now you only have " + inventory.pot.sugar + " units of flour, but you need three in there!")
 	}
-})
+	cookiecheck()
+});
+
 /*console.log all variables*/
-$('.button').click(function(){	
+$('.button').click(function(){
+	cookiecheck();
 	console.log("inventory flour: " + inventory.ingredients.flour + ", inventory sugar: " + inventory.ingredients.sugar + ", pot flour: " + inventory.pot.flour + ", pot sugar: " + inventory.pot.sugar + ", money: " + inventory.product.money + ", cookies: " + inventory.product.cookies)
-})
+});
+
 /*font switcher*/
 var fontcount = 0;
 $('#font').click(function(){
 	switch(fontcount) {
     case 0:
-  		$(".fontswitch").css("font-family","Chewy")
+  		$("h2").css("font-family","Chewy")
         break;
     case 1:
-  		$(".fontswitch").css("font-family","Pacifico")
+  		$("h2").css("font-family","Pacifico")
         break;
     case 2:
-    	$(".fontswitch").css("font-family","Shadows Into Light")
+    	$("h2").css("font-family","Shadows Into Light")
     	break;
     case 3:
-    	$(".fontswitch").css("font-family","Gloria Hallelujah")
+    	$("h2").css("font-family","Gloria Hallelujah")
     	break;
     case 4:
-    	$(".fontswitch").css("font-family","Indie Flower")
+    	$("h2").css("font-family","Indie Flower")
     	break;
     case 5:
-    	$(".fontswitch").css("font-family","Lobster")
+    	$("h2").css("font-family","Lobster")
+    	fontcount = 0
     	break;
     case 6:
-    	fontcount=0
+    	fontcount = 0
+    	console.log("fountcount reset to 0")
+
     default:
         console.log("error with font counter") //Shadows+Into+Light|Gloria+Hallelujah|Indie+Flower|Chewy|Pacifico
 }
 	fontcount++
-})
+});
 
 });
