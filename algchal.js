582d5f3834015a4eabb770f796b5ab08e366a749
Kyle Baker <finesttype@gmail.com>
2015-02-02 02:03:15 -0600

    looking pretty badass
    working on verbose version of algorithm, significant visual mods,
    attempt to make algchal window look like a terminal, background color is
    now blue.

--- algchal.js
+++ algchal.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    /*Algorithm proper*/
-    var result = null;
+    /*Algorithm proper
     var findSmallestDifference = function(arr) {
         var differences = [];
         arr.sort(function(a,b){return a - b});
@@ -9,6 +8,7 @@ $(document).ready(function(){
                 differences[differences.length] = arr[b] - arr[a];
             } 
         }
+        console.log(differences)
         var answer = (differences[0] + differences[1])
         for (d=0;d<differences.length;d++){
                 if (differences[d] < differences[d+1]) {
@@ -20,7 +20,31 @@ $(document).ready(function(){
         }
         return answer;
     };
-    /*end*/
+    end*/
+    /*Modified to be more verbose*/
+    var findSmallestDifference = function(arr) {
+        var differences = [];
+        arr.sort(function(a,b){return a - b});
+        $('#algchalbutton').after('<p>sorting numbers from greatest to least</p>')
+        $('#algchalbutton').after('<p>sorted: ' + arr + '</p>')
+        for(a=0;a<arr.length;a++){
+            for(b=a+1;b<arr.length;b++){
+                differences[differences.length] = arr[b] - arr[a];
+                $('#algchalbutton').after('<p>' + arr[b] + ' - ' + arr[a] + ' = ' + (arr[b]-arr[a]) + '</p>')
+            } 
+        }
+        console.log(differences)
+        var answer = (differences[0] + differences[1])
+        for (d=0;d<differences.length;d++){
+                if (differences[d] < differences[d+1]) {
+                        var bestOfTwo = differences[d]
+                        if (bestOfTwo < answer) {
+                            answer = bestOfTwo;
+                        }
+                }
+        }
+        return answer;
+    };
     /*start jquery*/
     var algArray = [];
     $('#algchalbutton').click(function(){
@@ -30,7 +54,7 @@ $(document).ready(function(){
             var tempvar = prompt("Enter a positive or negative integer to add to the array of numbers to be compared, or enter \"done\" to finish.");
             if (tempvar == "done"){
                 console.log(algArray)
-                document.getElementById("answer").innerHTML = "The smallest difference between that group of numbers is " + findSmallestDifference(algArray);
+                document.getElementById("answer").innerHTML = "<hr><b>The smallest difference between that group of numbers is " + findSmallestDifference(algArray) + "</b>";
             }
             else if (isNaN(tempvar)) {
                 console.log(typeof tempvar)
@@ -43,5 +67,4 @@ $(document).ready(function(){
         }
     })
 })
-
 /* || "\"done\""*/
