891fe138edff7afa57e4595d6bdf2e45c970284d
Kyle Baker <finesttype@gmail.com>
2015-03-04 06:09:46 -0600

    Major Update: Algchal now uses a text box
    Should be fully robust, except that 'enter' key does nothing, must use
    on-page button. algchal should have old code cleaned up, currently still
    there just commented out.

--- algchal.js
+++ algchal.js
@@ -9,7 +9,7 @@ var findSmallestDifference = function(arr) {
     $('#theline').before('<p id="cal">$> Determining difference of each pair of closest numbers:</p>');
     while(i+1<arr.length){
         (arr[i+1] - arr[i]) < answer ? answer = (arr[i+1] - arr[i]) : null;
-        $('#theline').before('<p id="cal">$> ' + arr[i+1] + ' - ' + arr[i] + ' = ' + (arr[i+1]-arr[i]) + '</p>');
+        $('#theline').before('<p id="cal">$> ' + arr[i+1] + ' - ' + arr[i] + ' = ' + (arr[i+1]-arr[i]) + ", so the current best answer is " + answer+ '</p>');
         if(answer === 0){$('#theline').before('<p id="cal">$> Found 0, no smaller possible, aborting.</p>');/*linecount++;*/ return 0;}
         //linecount++
         i++;
@@ -19,20 +19,36 @@ var findSmallestDifference = function(arr) {
     /*UX script*/
     //var clearvar = 0
     $('#algchalbutton').click(function(){
+        var rejectArray = [], testArray = $('input[name=algchalnum]').val().split(",");
+        testArray.forEach(function(val){if (!$.isNumeric(val)) rejectArray.push(val)});
+        testArray = testArray.map(Number);
+        testArray1 = testArray.filter(function(val){
+            return $.isNumeric(val)
+        });
         var algArray = [], tempvar = null, i=0;
+        if (testArray.length<2) {
+            document.getElementById("answer").innerHTML = "<b>Aborted because less than 2 numbers were entered.</b>";
+            $('#answer').fadeTo(500,1); $('#answer').fadeTo(500,.1); $('#answer').fadeTo(1000,1);
+        }
+        else {
+            document.getElementById("answer").innerHTML = "<b>The smallest difference between those numbers is: " + findSmallestDifference(testArray1) + "</b><br><br>" + "<p> (The following is/are invalid entrie(s): " + rejectArray + ")</p>";
+            $('#answer').fadeTo(500,1); $('#answer').fadeTo(500,.1); $('#answer').fadeTo(1000,1);
+        }        
         /*clean up old verbose output if run more than once
         if (clearvar > 0){for(a=0;a<linecount;a++){document.getElementById("cal").remove(this);}document.getElementById("templine").remove(this);linecount=3}
         clearvar++
         prompt script*/
+        /*
         while(tempvar!==""){
             tempvar = prompt("Enter a positive or negative integer to add to the array of numbers to be compared, or enter with the field blank to finish.");
             if (tempvar == ""){
-                if (algArray.length<2) {alert("You need to compare at least 2 numbers..."); tempvar = null;}
+                if (algArray.length<2) {
+                    document.getElementById("answer").innerHTML = "<b>Aborted because less than 2 numbers were entered.</b>";
+                    $('#answer').fadeTo(500,1); $('#answer').fadeTo(500,.1); $('#answer').fadeTo(1000,1);
+                }
                 else {
                     document.getElementById("answer").innerHTML = "<b>The smallest difference between those numbers is: " + findSmallestDifference(algArray) + "<b>";
-                    $('#answer').fadeTo(500,1);
-                    $('#answer').fadeTo(500,.1);
-                    $('#answer').fadeTo(1000,1);
+                    $('#answer').fadeTo(500,1); $('#answer').fadeTo(500,.1); $('#answer').fadeTo(1000,1);
                 }
             }
             else if (tempvar.indexOf(" ") > -1){alert("Please be sure not to enter any spaces in your answer.");}
@@ -45,6 +61,9 @@ var findSmallestDifference = function(arr) {
                 i++;
             }
         }
+        */
     });
+
+
 });
 /* || "\"done\""*/
